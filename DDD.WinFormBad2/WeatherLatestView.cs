067fbab442bab91cc05e7f4482d7d926228d4255
kero1976 <kero1976@outlook.jp>
2020-07-11 08:25:54 +0900

    Bad2作成

--- DDD.WinFormBad2/WeatherLatestView.cs
+++ DDD.WinFormBad2/WeatherLatestView.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DDD.WinForm.Common;
+using DDD.WinForm.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +15,6 @@ namespace DDD.WinForm
 {
     public partial class WeatherLatestView : Form
     {
-        private readonly string ConnectionString = @"Data Source=..\..\..\DDD.db;Version=3;";
         public WeatherLatestView()
         {
             InitializeComponent();
@@ -21,41 +22,15 @@ namespace DDD.WinForm
 
         private void LatestBtn_Click(object sender, EventArgs e)
         {
-            String sql = @"
-select DataDate,
-    Condition,
-    Temperature
-from Weather
-where AreaId = @AreaId
-order by DataDate desc
-LIMIT 1";
-            DataTable dt = new DataTable();
-            using (var connection = new SQLiteConnection(ConnectionString))
-            {
-                using (var command = new SQLiteCommand(sql, connection))
-                {
-                    connection.Open();
-                    command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
-                    using (var adapter = new SQLiteDataAdapter(command))
-                    {
-                        adapter.Fill(dt);
-                    }
-                }
-            }
+            var dt = WeatherSQLite.GetLatest(Convert.ToInt32(AreaIdTextBox.Text));
 
             if(dt.Rows.Count > 0)
             {
                 DataDateLabel.Text = dt.Rows[0]["DataDate"].ToString();
                 ConditionLabel.Text = dt.Rows[0]["Condition"].ToString();
                 TemperatureLabel.Text =
-                    RoundString(Convert.ToSingle(dt.Rows[0]["Temperature"].ToString()), 2) + "℃";
+                    CommonFunc.RoundString(Convert.ToSingle(dt.Rows[0]["Temperature"].ToString()), CommonConst.TemperarureDecimalPoint) + CommonConst.TemperatureUnitName;
             }
         }
-
-        private string RoundString(float value, int decimalPoint){
-            var temp = Convert.ToSingle(Math.Round(value, decimalPoint));
-            return temp.ToString("F" + decimalPoint);
-        }
-
     }
 }
