548242874fdcf1952a4f17b553c21f83070db89e
kero1976 <kero1976@outlook.jp>
2020-07-11 10:28:53 +0900

    DDD準備

--- DDD.WinForm/WeatherLatestView.Designer.cs
+++ DDD.WinForm/WeatherLatestView.Designer.cs
@@ -134,7 +134,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "WeatherLatestView";
-            this.Text = "WeatherLatestViewBad2";
+            this.Text = "WeatherLatestView";
             this.ResumeLayout(false);
             this.PerformLayout();
 
