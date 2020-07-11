d36dbdce08a821a4cb4c5b83d13dd1f4b794b40f
kero1976 <kero1976@outlook.jp>
2020-07-11 10:42:50 +0900

    プロジェクトを追加 参照を追加

---
+++ DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherLatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+        }
+    }
+}
